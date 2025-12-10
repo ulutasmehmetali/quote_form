bbdf25918da26408909a875488ad8b1669adef47
ulutassmehmetali <ulutassmehmetali@gmail.com>
2025-12-10 15:29:27 +0300

    ev

--- src/components/QuoteForm.tsx
+++ src/components/QuoteForm.tsx
@@ -11,7 +11,7 @@ import HeroSection from './HeroSection';
 import StepIndicator from './StepIndicator';
 import { apiUrl } from '../lib/api';
 
-const GOOGLE_SHEET_ENDPOINT = 'https://script.google.com/macros/s/AKfycby6Q-m6fOcNrCwwe8cyuG4cIL_JSbEgnnEvrTUcO8l1HFA_XFabECXZCjF1NUrP2XWFsg/exec';
+const GOOGLE_SHEET_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyuhjVSv1hE-YVyf6Pbl5Dhztt84m3eGJppOO1H0dHMFaaXFs0ll1naYzoHVVJ8tauN3g/exec';
 const STORAGE_KEY = 'miyomint_form_draft';
 const DRAFT_ENDPOINT = apiUrl('/api/draft');
 const INCOMPLETE_ENDPOINT = apiUrl('/api/incomplete');
