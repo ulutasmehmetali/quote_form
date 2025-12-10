8ead9d4bc47206c0520089516f940fd7eafa6eae
ulutassmehmetali <ulutassmehmetali@gmail.com>
2025-12-10 10:46:44 +0300

    ev

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -169,7 +169,7 @@ export default function ThankYou() {
               <div className="flex flex-col sm:flex-row gap-3 justify-center pt-3">
                 <Button
                   size="lg"
-                  onClick={() => window.location.reload()}
+                  onClick={() => (window.location.href = '/')}
                   className="sm:w-auto w-full"
                 >
                   <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
